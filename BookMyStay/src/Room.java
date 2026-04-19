eb1f8429ed97b070c092bf748d0fd123aab072f8
SakshamNeupane-01 <neupanasakcham@gmail.com>
2026-04-19 19:18:13 +0530

    use case 2 - room initialization Completed

---
+++ BookMyStay/src/Room.java
@@ -0,0 +1,25 @@
+/**
+ * this is an abstract class
+ *
+ * it represents a generic hotel room
+ */
+public abstract class Room {
+    protected int numberOfBeds;
+    protected int  squareFeet;
+    protected double pricePerNight;
+
+    public Room(int numberOfBeds,int squareFeet, double pricePerNight){
+        this.numberOfBeds=numberOfBeds;
+        this.squareFeet=squareFeet;
+        this.pricePerNight=pricePerNight;
+    }
+
+    public void displayRoom(){
+        System.out.println("Beds: "+numberOfBeds);
+        System.out.println(squareFeet+ " square Feet");
+        System.out.println("Price Per Night: "+pricePerNight);
+
+    }
+
+
+}
---
+++ BookMyStay/src/SingleRoom.java
@@ -0,0 +1,8 @@
+/**
+ * this class represent single room in the hotel
+ */
+public class SingleRoom extends Room{
+    public SingleRoom() {
+        super(1,250,1500.0);
+    }
+}
